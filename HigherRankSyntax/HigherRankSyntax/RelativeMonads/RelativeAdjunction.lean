d1fe61c66dc04bd0b1e44b5b190d9b8eeec3e47a
Noah Loutchmia <noah.loutchmia@ens-paris-saclay.fr>
2025-04-08 13:21:31 +0200

    (WIP) Fixing errors in the file  for relative adjunctions.

--- HigherRankSyntax/HigherRankSyntax/RelativeMonads/RelativeAdjunction.lean
+++ HigherRankSyntax/HigherRankSyntax/RelativeMonads/RelativeAdjunction.lean
@@ -2,29 +2,35 @@ import Mathlib.CategoryTheory.Category.Basic
 import Mathlib.CategoryTheory.Comma.Presheaf.Basic
 import Lean.Level
 import Mathlib.CategoryTheory.Monad.Basic
+import Mathlib.CategoryTheory.Products.Basic
+import Mathlib.CategoryTheory.Types
 import HigherRankSyntax.RelativeMonads.RelativeMonad
 
 open CategoryTheory
+open Opposite
 
 section
 universe u₁ u₂ u₃ v₁ v₂ v₃ w₁ w₂
 
   variable {A : Type u₁} [Category.{v₁} A]
-  variable {C : Type u₂} [Category.{v₂} C]
-  variable {J : A ⥤ C}
-  variable {D : Type u₃} [Category.{v₃} D]
+  variable {E : Type u₂} [Category.{v₂} E]
+  variable {J : A ⥤ E}
+  variable {C : Type u₃} [Category.{v₃} C]
 
-  variable (L : A ⥤ D)
-  variable (R : D ⥤ C)
+  variable (L : A ⥤ C)
+  variable (R : C ⥤ E)
 
 /- The following definition of a relative adjunction
   is based on the paper "Monads need not be endofunctors"
   (Altenkirch, Chapman & Uustalu). -/
 
-  def LeftHomFun : Cᵒᵖ × A ⥤ Type w₁ :=
-  sorry
+  def LeftHomFun : Aᵒᵖ × C ⥤ Type v₃ where
+    obj p :=   ((L.obj (unop p.1)) ⟶ (p.2))
+    map := by
+      intros x y f h
+      /- have g := f.1.unop ≫ h ≫ f.2 -/
 
-  def RightHomFun : Cᵒᵖ × A ⥤ Type w₂ :=
+  def RightHomFun : Aᵒᵖ × C ⥤ Type w₂ :=
   sorry
 
   structure RelativeAdjunction (L : A ⥤ D) (R : D ⥤ C) where
