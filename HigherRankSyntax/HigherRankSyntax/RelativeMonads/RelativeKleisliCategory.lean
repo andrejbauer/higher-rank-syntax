6a8de5df32c0d2a21ce9cd0243e775f916c6b939
Noah Loutchmia <noah.loutchmia@ens-paris-saclay.fr>
2025-04-25 13:03:59 +0200

    First attempt at defining relative Kleisli categories (does not work - issues with levels)

---
+++ HigherRankSyntax/HigherRankSyntax/RelativeMonads/RelativeKleisliCategory.lean
@@ -0,0 +1,35 @@
+import Mathlib.CategoryTheory.Category.Basic
+import Mathlib.CategoryTheory.Comma.Presheaf.Basic
+import Lean.Level
+import Mathlib.CategoryTheory.Monad.Basic
+import HigherRankSyntax.RelativeMonads.RelativeMonad
+
+open CategoryTheory
+
+
+section
+universe u₁ u₂ v₁ v₂ u v
+  variable {A : Type u₁} [Category.{v₁} A]
+  variable {E : Type u₂} [Category.{v₂} E]
+  variable {J : A ⥤ E}
+  variable (T : RelativeMonad J)
+
+-- The definition still has level issues
+  instance RelativeKleisliCategory : Category.{(max v₁ v₂ u₁ u₂) + 1, u₁} A where
+    Hom {X Y} :=  (J.obj X) ⟶ (T.map Y)
+    id X := (T.η X)
+    comp {X Y Z} f g := f ≫ (T.lift g)
+    id_comp {X Y} f := by simp ; rw[<-(T.unit_left f)]
+    comp_id {X Y} f := by
+       simp
+       have prp := (Category.comp_id f)
+       -- this is a bit messy, i should clean it
+       rw[<-prp, Category.assoc, whisker_eq]
+       rw[T.unit_right Y]
+       simp
+    assoc {X Y Z W} f g h := by
+      simp
+      -- If I don't add the levels of the category, the following line works,
+      -- but not the definition as a whole.
+      rw[T.comp_lift]
+end
