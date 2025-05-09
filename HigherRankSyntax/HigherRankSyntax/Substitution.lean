e48b4634173c8849c75b4aee0317cda540b0ac41
Noah Loutchmia <noah.loutchmia@ens-paris-saclay.fr>
2025-05-09 15:52:15 +0200

    Attempt at defining the unit of the relative monad (not complete) and adapting the code to the new definition of Expr.

--- HigherRankSyntax/HigherRankSyntax/Substitution.lean
+++ HigherRankSyntax/HigherRankSyntax/Substitution.lean
@@ -2,16 +2,16 @@
 import HigherRankSyntax.Syntax
 import HigherRankSyntax.Renaming
 
-def Substitution γ δ := ∀ {{α}}, Var α γ → Expr (δ ⊕ α)
+def Substitution γ δ := ∀ {{α}}, Var α γ → Expr δ α
 infix:25 " →ˢ " => Substitution
 
 namespace Substitution
 
 /-- The identity substutition -/
 def id {γ} : γ →ˢ γ :=
-  fun {α} x => x.varLeft ◃ (fun {β} (y : Var β α) => ⟦ Var.varRight ʳ⇑ _ ⟧ʳ (id y))
+  fun {α} x => x ◃ (fun {β} (y : Var β α) => (id (.varRight y)))
 termination_by γ.rank
-decreasing_by apply rank_Var_lt ; assumption
+decreasing_by apply rank_Var_lt ; sorry
 
 @[inherit_doc]
 notation " 𝟙ˢ " => Substitution.id
@@ -23,7 +23,7 @@ def lift {γ δ} (f : γ →ʳ δ) : γ →ˢ δ :=
 /-- Extend a substutution on the right. This is generally used when
     a substitution needs to be used under a binder. -/
 def extend {γ δ} (u : γ →ˢ δ) η : γ ⊕ η →ˢ δ ⊕ η
-| _, .varLeft x => ⟦ Var.varLeft ʳ⇑ _ ⟧ʳ (u x)
+| _, .varLeft x =>  ⟦.varLeft ⟧ʳ (u x)
 | _, .varRight y => 𝟙ˢ y.varRight
 
 @[inherit_doc]
@@ -31,7 +31,7 @@ infixl:95 " ⇑ˢ " => Substitution.extend
 
 /-- Compose a renaming and a substutition. -/
 def renaming_comp {α β γ} (f : β →ʳ γ) (u : α →ˢ β) : α →ˢ γ :=
-  fun ⦃δ⦄ x => ⟦ f ʳ⇑ δ ⟧ʳ u x
+  fun ⦃δ⦄ x => ⟦ f ⟧ʳ u x
 
 @[inherit_doc]
 infixr:95 " ʳ∘ˢ " => Substitution.renaming_comp
@@ -51,59 +51,73 @@ def sum {α β γ} (u : α →ˢ γ) (v : β →ˢ γ) : α ⊕ β →ˢ γ
 @[inherit_doc]
 infix:30 " ⊕ˢ " => Substitution.sum
 
+/- The following definitions are obsolete because the definition of Expr was changed,
+`act_left` and `act_right` had essencially the same role as
+`actFree` and `actBound` (which are now the ones in use). -/
+
 mutual
 
-def act_middle {α β γ δ : Shape} (u : β →ˢ α ⊕ γ) : Expr ((α ⊕ β) ⊕ δ) → Expr ((α ⊕ γ) ⊕ δ)
-  | .varLeft (.varLeft x) ◃ ts => .varLeft (.varLeft x) ◃ (fun ⦃_⦄ y => ⟦ .assocLeft ⟧ʳ act_middle u (⟦ .assocRight ⟧ʳ ts y))
-  | .varLeft (.varRight x) ◃ ts => act_right (fun ⦃_⦄ y => ⟦ .assocLeft ⟧ʳ act_middle u (⟦ .assocRight ⟧ʳ ts y)) (u x)
-  | .varRight x ◃ ts => .varRight x ◃ (fun ⦃_⦄ y => ⟦ .assocLeft ⟧ʳ act_middle u (⟦ .assocRight ⟧ʳ ts y))
-termination_by e => (β.rank, Expr.sizeOf e)
-decreasing_by
-  · apply Prod.Lex.right ; rw [Renaming.eq_size] ; apply Expr.sizeOfArg
-  · apply Prod.Lex.right ; rw [Renaming.eq_size] ; apply Expr.sizeOfArg
-  · apply Prod.Lex.left ; apply rank_Var_lt x
-  · apply Prod.Lex.right ; rw [Renaming.eq_size] ; apply Expr.sizeOfArg
+-- def act_middle {α β γ δ : Shape} (u : β →ˢ α ⊕ γ) : Expr ((α ⊕ β) ⊕ δ) → Expr ((α ⊕ γ) ⊕ δ)
+--   | .varLeft (.varLeft x) ◃ ts => .varLeft (.varLeft x) ◃ (fun ⦃_⦄ y => ⟦ .assocLeft ⟧ʳ act_middle u (⟦ .assocRight ⟧ʳ ts y))
+--   | .varLeft (.varRight x) ◃ ts => act_right (fun ⦃_⦄ y => ⟦ .assocLeft ⟧ʳ act_middle u (⟦ .assocRight ⟧ʳ ts y)) (u x)
+--   | .varRight x ◃ ts => .varRight x ◃ (fun ⦃_⦄ y => ⟦ .assocLeft ⟧ʳ act_middle u (⟦ .assocRight ⟧ʳ ts y))
+-- termination_by e => (β.rank, Expr.sizeOf e)
+-- decreasing_by
+--   · apply Prod.Lex.right ; rw [Renaming.eq_size] ; apply Expr.sizeOfArg
+--   · apply Prod.Lex.right ; rw [Renaming.eq_size] ; apply Expr.sizeOfArg
+--   · apply Prod.Lex.left ; apply rank_Var_lt x
+--   · apply Prod.Lex.right ; rw [Renaming.eq_size] ; apply Expr.sizeOfArg
 
-def act_left {α β γ} (u : α →ˢ β): Expr (α ⊕ γ) → Expr (β ⊕ γ)
-  | .varLeft x ◃ ts => act_right (fun ⦃_⦄ y => ⟦ .assocLeft ⟧ʳ act_left u (⟦ .assocRight ⟧ʳ ts y)) (u x)
-  | .varRight x ◃ ts => .varRight x ◃ (fun ⦃_⦄ y => ⟦ .assocLeft ⟧ʳ act_left u (⟦ .assocRight ⟧ʳ ts y))
-termination_by e => (α.rank, Expr.sizeOf e)
-decreasing_by
-  · apply Prod.Lex.right ; rw [Renaming.eq_size] ; apply Expr.sizeOfArg
-  · apply Prod.Lex.right ; rw [Renaming.eq_size] ; apply Expr.sizeOfArg
+-- def act_left {α β γ} (u : α →ˢ β): Expr (α ⊕ γ) → Expr (β ⊕ γ)
+--   | .varLeft x ◃ ts => act_right (fun ⦃_⦄ y => ⟦ .assocLeft ⟧ʳ act_left u (⟦ .assocRight ⟧ʳ ts y)) (u x)
+--   | .varRight x ◃ ts => .varRight x ◃ (fun ⦃_⦄ y => ⟦ .assocLeft ⟧ʳ act_left u (⟦ .assocRight ⟧ʳ ts y))
+-- termination_by e => (α.rank, Expr.sizeOf e)
+-- decreasing_by
+--   · apply Prod.Lex.right ; rw [Renaming.eq_size] ; apply Expr.sizeOfArg
+--   · apply Prod.Lex.right ; rw [Renaming.eq_size] ; apply Expr.sizeOfArg
 
-def act_right {α β γ} (u : β →ˢ α ⊕ γ) : Expr (α ⊕ β) → Expr (α ⊕ γ)
-  | .varLeft x ◃ ts => .varLeft x ◃ (fun ⦃_⦄ y => act_middle u (ts y))
-  | .varRight x ◃ ts => inst (fun ⦃_⦄ y => act_middle u (ts y)) (u x)
-termination_by e => (β.rank, Expr.sizeOf e)
-decreasing_by
-  · apply Prod.Lex.right ; apply Expr.sizeOfArg
-  · apply Prod.Lex.right ; apply Expr.sizeOfArg
-  · apply Prod.Lex.left ; apply rank_Var_lt x
+-- def act_right {α β γ} (u : β →ˢ α ⊕ γ) : Expr (α ⊕ β) → Expr (α ⊕ γ)
+--   | .varLeft x ◃ ts => .varLeft x ◃ (fun ⦃_⦄ y => act_middle u (ts y))
+--   | .varRight x ◃ ts => inst (fun ⦃_⦄ y => act_middle u (ts y)) (u x)
+-- termination_by e => (β.rank, Expr.sizeOf e)
+-- decreasing_by
+--   · apply Prod.Lex.right ; apply Expr.sizeOfArg
+--   · apply Prod.Lex.right ; apply Expr.sizeOfArg
+--   · apply Prod.Lex.left ; apply rank_Var_lt x
 
-def inst_left {α β γ} (u : β →ˢ α) : Expr ((α ⊕ β) ⊕ γ) → Expr (α ⊕ γ)
-  | .varLeft (.varLeft x) ◃ ts => .varLeft x ◃ (fun ⦃δ⦄ y => ⟦ .assocLeft ⟧ʳ inst_left u (⟦ .assocRight ⟧ʳ ts y))
-  | .varLeft (.varRight x) ◃ ts => act_right (fun ⦃_⦄ y => ⟦ .assocLeft ⟧ʳ inst_left u (⟦ .assocRight ⟧ʳ ts y)) (u x)
-  | .varRight x ◃ ts => .varRight x ◃ (fun ⦃_⦄ y => ⟦ .assocLeft ⟧ʳ inst_left u (⟦ .assocRight ⟧ʳ ts y))
-termination_by e => (β.rank, Expr.sizeOf e)
-decreasing_by
-  · apply Prod.Lex.right ; rw [Renaming.eq_size] ; apply Expr.sizeOfArg
-  · apply Prod.Lex.right ; rw [Renaming.eq_size] ; apply Expr.sizeOfArg
-  · apply Prod.Lex.left ; apply rank_Var_lt x
-  · apply Prod.Lex.right ; rw [Renaming.eq_size] ; apply Expr.sizeOfArg
 
-def inst {α β} (u : β →ˢ α) : Expr (α ⊕ β) → Expr α
-  | .varLeft x ◃ ts => x ◃ (fun ⦃_⦄ y => inst_left u (ts y))
-  | .varRight x ◃ ts => inst (fun ⦃_⦄ y => inst_left u (ts y)) (u x)
-termination_by e => (β.rank, Expr.sizeOf e)
-decreasing_by
-  · apply Prod.Lex.right ; apply Expr.sizeOfArg
-  · apply Prod.Lex.right ; apply Expr.sizeOfArg
-  · apply Prod.Lex.left ; apply rank_Var_lt x
+
+
+/- To do : Adapt the instanciation functions, as they are needed to define act.-/
+
+-- def inst_left {α β γ} (u : β →ˢ α) : Expr ((α ⊕ β) ⊕ γ) → Expr (α ⊕ γ)
+--   | .varLeft (.varLeft x) ◃ ts => .varLeft x ◃ (fun ⦃δ⦄ y => ⟦ .assocLeft ⟧ʳ inst_left u (⟦ .assocRight ⟧ʳ ts y))
+--   | .varLeft (.varRight x) ◃ ts => act_right (fun ⦃_⦄ y => ⟦ .assocLeft ⟧ʳ inst_left u (⟦ .assocRight ⟧ʳ ts y)) (u x)
+--   | .varRight x ◃ ts => .varRight x ◃ (fun ⦃_⦄ y => ⟦ .assocLeft ⟧ʳ inst_left u (⟦ .assocRight ⟧ʳ ts y))
+-- termination_by e => (β.rank, Expr.sizeOf e)
+-- decreasing_by
+--   · apply Prod.Lex.right ; rw [Renaming.eq_size] ; apply Expr.sizeOfArg
+--   · apply Prod.Lex.right ; rw [Renaming.eq_size] ; apply Expr.sizeOfArg
+--   · apply Prod.Lex.left ; apply rank_Var_lt x
+--   · apply Prod.Lex.right ; rw [Renaming.eq_size] ; apply Expr.sizeOfArg
+
+-- def inst {α β} (u : β →ˢ α) : Expr (α ⊕ β) → Expr α
+--   | .varLeft x ◃ ts => x ◃ (fun ⦃_⦄ y => inst_left u (ts y))
+--   | .varRight x ◃ ts => inst (fun ⦃_⦄ y => inst_left u (ts y)) (u x)
+-- termination_by e => (β.rank, Expr.sizeOf e)
+-- decreasing_by
+--   · apply Prod.Lex.right ; apply Expr.sizeOfArg
+--   · apply Prod.Lex.right ; apply Expr.sizeOfArg
+--   · apply Prod.Lex.left ; apply rank_Var_lt x
+
+
 
 /-- The action of a substutition on an expression -/
-def act {α β} (u : α →ˢ β): Expr α → Expr β
-| x ◃ ts => inst (fun ⦃_⦄ y => act_left u (ts y)) (u x)
+def act {α β δ} (u : α →ˢ β) : (Expr α δ) → (Expr β δ)
+| x ◃ ts => sorry
+  -- inst (fun ⦃_⦄ y => act_left u (ts y)) (u x)
+| x ◂ ts => sorry
+
 
 end -- mutual
 
@@ -112,7 +126,7 @@ notation:60 " ⟦" u "⟧ˢ " e:61 => Substitution.act u e
 
 /-- Composition of substitutions -/
 def comp {γ δ θ} (u : γ →ˢ δ) (v : δ →ˢ θ) : γ →ˢ θ
-| β, x => act (v ⇑ˢ β) (u x)
+| _, x => act v (u x)
 
 @[inherit_doc]
 notation:90 g:90 " ∘ˢ " f:91 => Substitution.comp f g
--- HigherRankSyntax/HigherRankSyntax/SyntaxRelativeMonad.lean
+++ HigherRankSyntax/HigherRankSyntax/SyntaxRelativeMonad.lean
@@ -1,12 +1,13 @@
-import Lean.Level
+
 import Mathlib.CategoryTheory.Category.Basic
 import Mathlib.CategoryTheory.Comma.Presheaf.Basic
+import Lean.Level
 import Mathlib.CategoryTheory.Monad.Basic
 
 import HigherRankSyntax.RelativeMonads.RelativeMonad
 import HigherRankSyntax.Renaming
 import HigherRankSyntax.Syntax
-import HigherRankSyntax.Substitution
+-- import HigherRankSyntax.Substitution
 
 def ArityCat := ShapeCat
 
@@ -17,18 +18,26 @@ structure VarRen (γ δ : Shape) : Type where
   var : Var arity γ
   ren : arity →ʳ δ
 
+def VartoVarRen {γ α} (x : Var α γ) : VarRen γ α where
+  arity := α
+  var := x
+  ren := 𝟙ʳ
+
 /-- The object part of the variables functor -/
+@[reducible]
 def 𝕁ₒ (γ : Shape) : Arity ⥤ Type 0 where
   obj := VarRen γ
   map := fun ρ xσ => { arity := xσ.arity, var := xσ.var, ren := ρ ∘ʳ xσ.ren }
 
 /-- The variables functor -/
+@[reducible]
 def 𝕁 : Shape ⥤ (Arity ⥤ Type 0) where
   obj := 𝕁ₒ
   map := fun {γ γ'} ρ =>
     { app := fun δ xσ => { arity := xσ.arity, var := ρ xσ.var, ren := xσ.ren } }
 
 /-- The object part of the expression functor -/
+@[reducible]
 def 𝕋ₒ (γ : Shape) : Arity ⥤ Type 0 where
 
   obj := Expr γ
@@ -49,7 +58,7 @@ def 𝕋ₒ (γ : Shape) : Arity ⥤ Type 0 where
 /-- The expressions functor -/
 def 𝕋 : Shape ⥤ (Arity ⥤ Type 0) where
 
-  obj := 𝕋
+  obj := 𝕋ₒ
 
   map := fun {γ γ'} f =>
     { app := fun _ => Renaming.actFree f
@@ -75,11 +84,33 @@ def 𝕋 : Shape ⥤ (Arity ⥤ Type 0) where
     apply Renaming.actFree.map_comp
 
 
+def η_val (γ : Shape) :  𝕁ₒ γ ⟶ 𝕋ₒ γ where
+  app δ := by
+    -- unfold 𝕁ₒ ; simp
+    -- unfold 𝕋ₒ ; simp
+    intro x
+    have temp1 β := (η_val x.arity).app β
+    simp [𝕁ₒ, 𝕋ₒ] at temp1
+    have ren_subterms : x.arity →ʳ γ ⊕ δ := ((γ ⇑ʳ x.ren) ∘ʳ .varRight)
+    have temp2 := x.var ◃ (fun β y => ⟦ren_subterms⟧ʳ (temp1 β (VartoVarRen y)))
+    exact temp2
+  naturality {δ δ'} r := by
+    simp
+    unfold Renaming.actBound
+
+
+    sorry
+-- prove termination
+--   ⦃β : Arity⦄ → Var β ρ.arity → Expr (γ ⊕ ?m.5168) β : TypeLean 4
+
+
 /-- The main goal is to define the relative monad for syntax.
     This should be in the same file as the first part of this file.  -/
 def SyntaxRelativeMonad : RelativeMonad 𝕁 := {
   map := 𝕋ₒ
-  η := sorry
+  η := fun X => by
+    unfold 𝕁 ; simp
+    sorry
   lift := sorry
   unit_left := sorry
   unit_right := sorry
